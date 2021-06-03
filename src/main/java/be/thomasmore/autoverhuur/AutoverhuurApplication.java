30c9e0d06c01a0953414969ee0751383384bfc1f
simenraes <simieraes@hotmail.com>
2021-06-03 15:15:26 +0200

    add initial files

---
+++ src/main/java/be/thomasmore/autoverhuur/AutoverhuurApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.autoverhuur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoverhuurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoverhuurApplication.class, args);
+    }
+
+}
